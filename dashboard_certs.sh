0bd1ec032584920a6c8571a2483bfa4d358e3da3
Igor <igor@devfire.io>
2020-02-22 10:40:12 -0500

    a bunch of files and tutorials

---
+++ dashboard_certs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mkdir certs
+openssl req -nodes -newkey rsa:2048 -keyout certs/dashboard.key -out certs/dashboard.csr -subj "/CN=kubernetes-dashboard"
+openssl x509 -req -sha256 -days 365 -in certs/dashboard.csr -signkey certs/dashboard.key -out certs/dashboard.crt
+/snap/bin/microk8s.kubectl create secret generic kubernetes-dashboard-certs --from-file=certs -n kube-system
+/snap/bin/microk8s.kubectl delete pod -n kubernetes-dashboard -l k8s-app=kubernetes-dashboard
